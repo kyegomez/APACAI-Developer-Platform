bcb0132c4490064fc147b62e55bb92a8e6fc2e61
Kye <kye@apacmediasolutions.com>
2023-07-31 11:39:57 -0400

    clean up

--- apps/app/pages/index.tsx
+++ apps/app/pages/index.tsx
@@ -10,7 +10,7 @@ export function Index() {
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome app 👋
+            Welcome to APAC AI's developer platform 👋
           </h1>
         </div>
 
@@ -50,7 +50,7 @@ export function Index() {
           <div id="learning-materials" className="rounded shadow">
             <h2>Learning materials</h2>
             <a
-              href="https://nx.dev/getting-started/intro?utm_source=nx-project"
+              href="https://APAC AI.dev/getting-started/intro?utm_source=APAC AI-project"
               target="_blank"
               rel="noreferrer"
               className="list-item-link"
@@ -87,7 +87,7 @@ export function Index() {
               </svg>
             </a>
             <a
-              href="https://blog.nrwl.io/?utm_source=nx-project"
+              href="https://blog.apac.ai"
               target="_blank"
               rel="noreferrer"
               className="list-item-link"
@@ -124,7 +124,7 @@ export function Index() {
               </svg>
             </a>
             <a
-              href="https://www.youtube.com/c/Nrwl_io/videos?utm_source=nx-project&sub_confirmation=1"
+              href="https://www.youtube.com/@kyegomez3242/videos"
               target="_blank"
               rel="noreferrer"
               className="list-item-link"
@@ -140,7 +140,7 @@ export function Index() {
               </svg>
               <span>
                 YouTube channel
-                <span> Nx Show, talks & tutorials </span>
+                <span> APAC AI Show, talks & tutorials </span>
               </span>
               <svg
                 fill="none"
@@ -157,7 +157,7 @@ export function Index() {
               </svg>
             </a>
             <a
-              href="https://nx.dev/react-tutorial/01-create-application?utm_source=nx-project"
+              href="https://APAC AI.dev/react-tutorial/01-create-application?utm_source=APAC AI-project"
               target="_blank"
               rel="noreferrer"
               className="list-item-link"
@@ -194,7 +194,7 @@ export function Index() {
               </svg>
             </a>
             <a
-              href="https://nxplaybook.com/?utm_source=nx-project"
+              href="https://APAC AIplaybook.com/?utm_source=APAC AI-project"
               target="_blank"
               rel="noreferrer"
               className="list-item-link"
@@ -216,7 +216,7 @@ export function Index() {
               </svg>
               <span>
                 Video courses
-                <span> Nx custom courses </span>
+                <span> APAC AI custom courses </span>
               </span>
               <svg
                 fill="none"
@@ -235,9 +235,9 @@ export function Index() {
           </div>
           <div id="other-links">
             <a
-              id="nx-console"
+              id="APAC AI-console"
               className="button-pill rounded shadow"
-              href="https://marketplace.visualstudio.com/items?itemName=nrwl.angular-console&utm_source=nx-project"
+              href="https://marketplace.visualstudio.com/items?itemName=nrwl.angular-console&utm_source=APAC AI-project"
               target="_blank"
               rel="noreferrer"
             >
@@ -251,11 +251,11 @@ export function Index() {
                 <path d="M23.15 2.587L18.21.21a1.494 1.494 0 0 0-1.705.29l-9.46 8.63-4.12-3.128a.999.999 0 0 0-1.276.057L.327 7.261A1 1 0 0 0 .326 8.74L3.899 12 .326 15.26a1 1 0 0 0 .001 1.479L1.65 17.94a.999.999 0 0 0 1.276.057l4.12-3.128 9.46 8.63a1.492 1.492 0 0 0 1.704.29l4.942-2.377A1.5 1.5 0 0 0 24 20.06V3.939a1.5 1.5 0 0 0-.85-1.352zm-5.146 14.861L10.826 12l7.178-5.448v10.896z" />
               </svg>
               <span>
-                Install Nx Console
+                Install APAC AI Console
                 <span>Plugin for VSCode</span>
               </span>
             </a>
-            <div id="nx-cloud" className="rounded shadow">
+            <div id="APAC AI-cloud" className="rounded shadow">
               <div>
                 <svg
                   viewBox="0 0 120 120"
@@ -272,7 +272,7 @@ export function Index() {
                   />
                 </svg>
                 <h2>
-                  NxCloud
+                  APAC AICloud
                   <span>Enable faster CI & better DX</span>
                 </h2>
               </div>
@@ -280,20 +280,20 @@ export function Index() {
                 You can activate distributed tasks executions and caching by
                 running:
               </p>
-              <pre>nx connect-to-nx-cloud</pre>
+              <pre>APAC AI connect-to-APAC AI-cloud</pre>
               <a
-                href="https://nx.app/?utm_source=nx-project"
+                href="https://APAC AI.app/?utm_source=APAC AI-project"
                 target="_blank"
                 rel="noreferrer"
               >
                 {' '}
-                What is Nx Cloud?{' '}
+                What is APAC AI Cloud?{' '}
               </a>
             </div>
             <a
-              id="nx-repo"
+              id="APAC AI-repo"
               className="button-pill rounded shadow"
-              href="https://github.com/nrwl/nx?utm_source=nx-project"
+              href="https://github.com/kyegomez/APACAI"
               target="_blank"
               rel="noreferrer"
             >
@@ -306,8 +306,8 @@ export function Index() {
                 <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
               </svg>
               <span>
-                Nx is open source
-                <span> Love Nx? Give us a star! </span>
+                APAC AI is open source
+                <span> Love APAC AI? Give us a star! </span>
               </span>
             </a>
           </div>
@@ -315,7 +315,7 @@ export function Index() {
 
         <div id="commands" className="rounded shadow">
           <h2>Next steps</h2>
-          <p>Here are some things you can do with Nx:</p>
+          <p>Here are some things you can do with APAC AI:</p>
           <details>
             <summary>
               <svg
@@ -334,10 +334,11 @@ export function Index() {
               Add UI library
             </summary>
             <pre>
-              <span># Generate UI lib</span>
-              nx g @nrwl/next:library ui
+              <span># Launch a Swarm</span>
+              pip install swarms
               <span># Add a component</span>
-              nx g @nrwl/next:component button --project=ui
+              from swarms import swarms
+              swarm("objective", "key")
             </pre>
           </details>
           <details>
@@ -357,7 +358,7 @@ export function Index() {
               </svg>
               View interactive project graph
             </summary>
-            <pre>nx graph</pre>
+            <pre>APAC AI graph</pre>
           </details>
           <details>
             <summary>
@@ -378,11 +379,11 @@ export function Index() {
             </summary>
             <pre>
               <span># see what&apos;s been affected by changes</span>
-              nx affected:graph
+              APAC AI affected:graph
               <span># run tests for current changes</span>
-              nx affected:test
+              APAC AI affected:test
               <span># run e2e tests for current changes</span>
-              nx affected:e2e
+              APAC AI affected:e2e
             </pre>
           </details>
         </div>
