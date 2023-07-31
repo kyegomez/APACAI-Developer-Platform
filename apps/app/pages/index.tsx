100b0c82db677622d5e5b0011ca53cd1443a0859
Kye <kye@apacmediasolutions.com>
2023-07-31 11:46:11 -0400

    clean up

--- apps/app/pages/index.tsx
+++ apps/app/pages/index.tsx
@@ -30,9 +30,9 @@ export function Index() {
                   d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
                 />
               </svg>
-              <span>You&apos;re up and running</span>
+              <span>Announcing Swarms, Kosmos, and Andromeda</span>
             </h2>
-            <a href="#commands"> What&apos;s next? </a>
+            <a href="#commands"> Documentation, examples, it's all here for you </a>
           </div>
           <div className="logo-container">
             <svg
